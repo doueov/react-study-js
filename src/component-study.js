a7aa8ad0da5f1b411c9d1965a004a49c6c903f7f
doueov <gitpdyeon@gmail.com>
2024-09-03 09:15:41 +0900

    props

--- src/component-study.js
+++ src/component-study.js
@@ -2,16 +2,23 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// 컴포넌트, props, useState
+// 컴포넌트
 function MyComponent() {
     return <div>Hello</div>
 }
 
+// props
 function MyPropsComponent(props) {
     return <div>{JSON.stringify(props)}</div>
+}
+
+// props로 name 값을 전달받아서 출력하는 NamePrint 컴포넌트 정의
+function NamePrint(props) {
+    return <div>{props.name} {props.age}</div>
 }
 
 root.render(<div>
     {MyPropsComponent({ a: "abc", b: 1234, c: { a: 100 } })}
     <MyPropsComponent a='abc' b={1234} c={{ a: 100 }} />
+    <NamePrint name="doyeon" age={19} obj={{a : 100}} fn={() => {}}/>
 </div>)
